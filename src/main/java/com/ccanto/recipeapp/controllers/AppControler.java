a75ae0f18fbd59ef6fab81be83e3e968e63aad2b
Catarina Canto <catarinaccanto@gmail.com>
2019-06-23 15:16:00 +0100

    small fix

--- src/main/java/com/ccanto/recipeapp/controllers/AppControler.java
+++ src/main/java/com/ccanto/recipeapp/controllers/AppControler.java
@@ -1,7 +1,6 @@
 package com.ccanto.recipeapp.controllers;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppControler {
 
-	@Value("${spring.application.name}")
-	private String appName;
-
 	@GetMapping("/")
 	public String homePage(Model model) {
-		model.addAttribute("appName", appName);
+		model.addAttribute("appName", "recipes");
 		return "home";
 	}
 
